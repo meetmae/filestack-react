243d1d7dacedf26c584e231cbb5d3e74f9116988
Anna Kopp <equatic@gmail.com>
2022-10-12 16:37:36 -0400

    upgrade

--- src/picker/use-picker.test.js
+++ src/picker/use-picker.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react';
 import usePicker from './use-picker';
 
 jest.mock('filestack-js', () => ({
